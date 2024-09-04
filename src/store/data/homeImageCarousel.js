917dbbea0c00fafba1cac0e5191e0a26a69fff44
Aadarsh Agarwal <aadarshsaroon001@gmail.com>
2024-09-05 00:51:48 +0530

    home page jumbodrop images updates

--- src/store/data/homeImageCarousel.js
+++ src/store/data/homeImageCarousel.js
@@ -3,15 +3,13 @@ import { createSlice } from "@reduxjs/toolkit";
 const images = createSlice({
   name: "homeImageCarousel",
   initialState: [
-    "1.jpg",
-    "image (1).jpg",
-    "image (2).jpg",
-    "image (3).jpg",
-    "image (4).jpg",
-    "image (5).jpg",
-    "image (6).jpg",
-    "image (7).jpg",
-    "image (8).jpg",
+    "main/image (1).jpg",
+    "main/image (2).jpg",
+    "main/image (3).jpeg",
+    "main/image (4).png",
+    "main/image (5).jpg",
+    "main/image (7).png",
+    "main/image (9).png",
   ],
 });
 export default images;
