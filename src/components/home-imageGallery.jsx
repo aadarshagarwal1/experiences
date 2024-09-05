af55ca3a7e939c7bea924c396fdf7459af1cdcb7
Aadarsh Agarwal <aadarshsaroon001@gmail.com>
2024-09-05 16:51:06 +0530

    home gallery images updated

--- src/components/home-imageGallery.jsx
+++ src/components/home-imageGallery.jsx
@@ -37,14 +37,14 @@ export default function HomeImageGallery() {
           >
             <div className="influencer-col-item">
               <img
-                src="https://assets.website-files.com/5b4f3cc33f2e3868c5a5b80c/5b7bd095bbb60b3135942d86_influencer1.jpg"
+                src="homeGallery/image (1).png"
                 alt="private_pool_villa_bali"
                 width="210"
               />
             </div>
             <div className="influencer-col-item">
               <img
-                src="https://assets.website-files.com/5b4f3cc33f2e3868c5a5b80c/5b7bd0f87e58f19305e4def2_influencer2.jpg"
+                src="homeGallery/image (2).png"
                 alt="uluwatu_villa_bali_influencer"
               />
             </div>
@@ -58,13 +58,13 @@ export default function HomeImageGallery() {
           >
             <div className="influencer-col-item">
               <img
-                src="https://assets.website-files.com/5b4f3cc33f2e3868c5a5b80c/5b7bd17bbbb60b6de0943055_influencer3.jpg"
+                src="homeGallery/image (3).png"
                 alt="uluwatu_villa_bali_influencer"
               />
             </div>
             <div className="influencer-col-item">
               <img
-                src="https://assets.website-files.com/5b4f3cc33f2e3868c5a5b80c/5b7bd1bcbbb60bab20943086_Influencer4.jpg"
+                src="homeGallery/image (4).png"
                 alt="romantic_bali_accommodation"
               />
             </div>
@@ -78,13 +78,13 @@ export default function HomeImageGallery() {
           >
             <div className="influencer-col-item">
               <img
-                src="https://assets.website-files.com/5b4f3cc33f2e3868c5a5b80c/5b7bd227c80be660312c2d87_Influencer5.jpg"
+                src="homeGallery/image (5).png"
                 alt="private_pool_villas_uluwatu"
               />
             </div>
             <div className="influencer-col-item">
               <img
-                src="https://assets.website-files.com/5b4f3cc33f2e3868c5a5b80c/5b7bd596a3bf8850bf5eb0d7_influencer6.jpg"
+                src="homeGallery/image (6).png"
                 alt="uluwatu_villas_king_sized_bed"
               />
             </div>
@@ -99,13 +99,13 @@ export default function HomeImageGallery() {
           >
             <div className="influencer-col-item">
               <img
-                src="https://assets.website-files.com/5b4f3cc33f2e3868c5a5b80c/5b7bd2aa8faeeb68f8a263dc_influencer7.jpg"
+                src="homeGallery/image (7).png"
                 alt="luxury_resort_villas_bukit_peninsula"
               />
             </div>
             <div className="influencer-col-item">
               <img
-                src="https://assets.website-files.com/5b4f3cc33f2e3868c5a5b80c/5b7bd2f57e58f1795ce4e372_influencer8.jpg"
+                src="homeGallery/image (8).png"
                 alt="private_uluwatu_villas_bali"
               />
             </div>
@@ -120,13 +120,13 @@ export default function HomeImageGallery() {
           >
             <div className="influencer-col-item">
               <img
-                src="https://assets.website-files.com/5b4f3cc33f2e3868c5a5b80c/5b7bd522586320419dc86f6d_Influencer9.jpg"
+                src="homeGallery/image (9).png"
                 alt="private_pool_villas_bali"
               />
             </div>
             <div className="influencer-col-item">
               <img
-                src="https://assets.website-files.com/5b4f3cc33f2e3868c5a5b80c/5b7bd39d5863206a0bc86ab2_Influencer10.jpg"
+                src="homeGallery/image (10).png"
                 alt="uluwatu_wedding_venue_bali"
               />
             </div>
